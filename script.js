1b9dfcf87cf29d6b42b5debd7875171074739e2a
Boran Acu <91663853+boranacu@users.noreply.github.com>
2025-08-30 01:20:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 600;
-canvas.height = 200;
+canvas.width = 1200;
+canvas.height = 400;
 
 // Load images
 const cowImg = new Image();
@@ -14,7 +14,7 @@ const bombImg = new Image();
 bombImg.src = "assets/cone.png";
 
 // Game variables
-let cow = { x: 50, y: 150, width: 40, height: 40, dy: 0, jump: false };
+let cow = { x: 50, y: 150, width: 80, height: 80, dy: 0, jump: false };
 let gravity = 0.6;
 let ground = 160;
 
@@ -130,3 +130,4 @@ document.getElementById("restartBtn").addEventListener("click", () => {
 });
 
 loop();
+
